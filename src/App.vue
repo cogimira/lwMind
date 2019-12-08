39fec5fca9d2a8bf7bbed20199fbcdbb54a5c302
wanghauiyao <741663036@qq.com>
2019-12-08 14:12:01 +0800

    command

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,24 @@ export default {
   mounted () {
     let canvas = document.getElementById("canvas");
     canvas.setAttribute("width", document.body.offsetWidth);
-    canvas.setAttribute("height", window.innerHeight + 100);
-    new GraphStage(canvas);
+    canvas.setAttribute("height", window.innerHeight);
+    let width = canvas.width,height=canvas.height;
+    if (window.devicePixelRatio) {
+
+                canvas.style.width = width + "px";
+                canvas.style.height = height + "px";
+                canvas.height = height * window.devicePixelRatio;
+                canvas.width = width * window.devicePixelRatio;
+                canvas.getContext("2d").scale(window.devicePixelRatio, window.devicePixelRatio);
+    }
+    // let width = canvas.offsetWidth;
+    // let height = canvas.offsetHeight;
+    // canvas.width = width * 2;
+    // canvas.height = height * 2;
+    // canvas.style.width = width + "px";
+    // canvas.style.height = height + "px";
+    let appContainer = document.getElementById("app");
+    new GraphStage(canvas, {container: appContainer});
   },
   components: {
     
@@ -30,6 +46,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  position: relative;
+  /* zoom: 0.5; */
   /* margin-top: 20px; */
 }
 </style>
